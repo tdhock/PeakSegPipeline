63de52b5f1697c4e36546d9f24f4a9ae05508a66
Toby Dylan Hocking <tdhock5@gmail.com>
2017-11-23 13:02:06 -0500

    int overflow test fails

--- tests/testthat/test-CRAN-coverage.R
+++ tests/testthat/test-CRAN-coverage.R
@@ -6,11 +6,9 @@ count.dt <- fread("
 chrom	chromStart	chromEnd	coverage
 chr1	0	10	1
 chr1	10	20	2
-chr1	30	40	1
+chr2	30	40	1
 ")
-chromInfo <- fread("
-chr1 100
-")
+chromInfo <- data.table(chrom=c("chr1", "chr2"), chromEnd=50)
 work.dir <- tempdir()
 chromInfo.txt <- file.path(work.dir, "chromInfo.txt")
 fwrite(chromInfo, chromInfo.txt, sep="\t", col.names=FALSE)
@@ -40,4 +38,20 @@ test_that("bigWigCoverage stats are OK", {
 test_that("intermediate file is deleted", {
   count.bedGraph <- file.path(work.dir, "count.bedGraph")
   expect_true(!file.exists(count.bedGraph))
+})
+
+## bigger genome example, int overflow.
+chromInfo <- data.table(
+  chrom=c("chr1", "chr2"),
+  chromEnd=.Machine$integer.max)
+fwrite(chromInfo, chromInfo.txt, sep="\t", col.names=FALSE)
+system.or.stop(paste(
+  "bedGraphToBigWig",
+  input.bedGraph,
+  chromInfo.txt,
+  count.bigWig))
+bigWig.dt <- bigWigCoverage(count.bigWig)
+test_that("no integer overflow for big chroms", {
+  expect_true(is.finite(bigWig.dt$total.bases))
+  expect_true(is.finite(bigWig.dt$mean.coverage))
 })
