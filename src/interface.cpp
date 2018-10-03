f3fd623872a03a78220eb3d8ba705a848bf3a69c
Toby Dylan Hocking <tdhock5@gmail.com>
2018-10-03 13:53:00 -0700

    remove FPOP 1 WARN

--- src/interface.cpp
+++ src/interface.cpp
@@ -1,47 +1,10 @@
 /* -*- compile-command: "R CMD INSTALL .." -*- */
 
-#include "PeakSegFPOPLog.h"
 #include "denormalize.h"
 #include "coverage.h"
 #include <R.h>
 #include <R_ext/Rdynload.h>
 
-void PeakSegFPOP_interface
-(char **file_vec, char **pen_vec){
-  char *bedGraph = file_vec[0];
-  char *penalty = pen_vec[0];
-  int status = PeakSegFPOP_disk(bedGraph, penalty);
-  if(status==ERROR_PENALTY_NOT_FINITE){
-    error("penalty=%s but must be finite", penalty);
-  }
-  if(status==ERROR_PENALTY_NEGATIVE){
-    error("penalty=%s must be non-negative", penalty);
-  }
-  if(status==ERROR_UNABLE_TO_OPEN_BEDGRAPH){
-    error("unable to open input file for reading %s", bedGraph);
-  }
-  if(status==ERROR_NOT_ENOUGH_COLUMNS){
-    error("each line of input data file %s should have exactly four columns", bedGraph);
-  }
-  if(status==ERROR_NON_INTEGER_DATA){
-    error("fourth column of input data file %s should be integer", bedGraph);
-  }
-  if(status==ERROR_INCONSISTENT_CHROMSTART_CHROMEND){
-    error("there should be no gaps (columns 2-3) in input data file %s", bedGraph);
-  }
-  if(status==ERROR_WRITING_COST_FUNCTIONS){
-    error("unable to write to cost function database file %s_penalty=%s.db",
-	  bedGraph, penalty);
-  }
-  if(status==ERROR_WRITING_LOSS_OUTPUT){
-    error("unable to write to loss output file %s_penalty=%s_loss.tsv",
-	  bedGraph, penalty);
-  }
-  if(status != 0){
-    error("error code %d", status);
-  }
-}
-  
 void denormalize_interface
 (char **bedGraph_file_vec, char **out_file_vec){
   int status = denormalize(bedGraph_file_vec[0], out_file_vec[0]);
@@ -86,10 +49,6 @@ void coverage_interface
 }
   
 R_CMethodDef cMethods[] = {
-  {"PeakSegFPOP_interface",
-   (DL_FUNC) &PeakSegFPOP_interface, 2
-   //,{REALSXP, REALSXP, INTSXP, INTSXP, REALSXP}
-  },
   {"denormalize_interface",
    (DL_FUNC) &denormalize_interface, 2
    //,{CHARSXP, CHARSXP}
