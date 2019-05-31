c299fec4a5efe7e869ce3bb03802cd75c4aac7a8
Toby Dylan Hocking <tdhock5@gmail.com>
2019-05-31 13:15:00 -0700

    cran tests pass on laptop

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 rm -rf PeakSegPipeline-release
 cp -r PeakSegPipeline PeakSegPipeline-release
 grep -v Remotes PeakSegPipeline/DESCRIPTION > PeakSegPipeline-release/DESCRIPTION
-rm PeakSegPipeline-release/tests/testthat/*
+rm PeakSegPipeline-release/tests/testthat/* PeakSegPipeline-release/*_chromInfo.txt*
 cp PeakSegPipeline/tests/testthat/test-CRAN*.R PeakSegPipeline-release/tests/testthat
 PKG_TGZ=$(R CMD build PeakSegPipeline-release|grep PeakSegPipeline_|sed "s/.* building [‘']//"|sed "s/['’].*//")
 R CMD INSTALL $PKG_TGZ
