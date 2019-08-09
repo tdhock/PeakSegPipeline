440cc6c3192035b3f76dd6cbdca2e7f09a3144ee
Toby Dylan Hocking <Toby.Hocking@nau.edu>
2019-08-09 10:49:58 -0700

    chr11-600 win path

--- tests/testthat/test-CRAN-target.R
+++ tests/testthat/test-CRAN-target.R
@@ -11,7 +11,7 @@ problem.dir <- file.path(
   "Mono1_H3K27ac",
   "S001YW_NCMLS",
   "problems",
-  "chr11:60000-580000")
+  "chr11-60000-580000")
 dir.create(problem.dir, recursive=TRUE, showWarnings=FALSE)
 problem.labels.bed <- file.path(problem.dir, "labels.bed")
 write.table(
